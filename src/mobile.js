e1719c6e321ffa73183be124656ce473fb327035
AshwinRames <90181750+AshwinRames@users.noreply.github.com>
2022-04-15 11:54:22 +0530

    Update mobile.js

--- src/mobile.js
+++ src/mobile.js
@@ -1,13 +1,14 @@
 //SmartPhone
-
-
-
-// AndroidPhone
-
-
-
-// IPhone
-
-
-
-//MobileCampus
+class SmartPhone{
+    constructor(ram, batteryPower, price){
+        this.ram=ram;
+        this.batteryPower=batteryPower;
+        this.price=price;
+    }
+    displayFeatures(){
+        return this.ram +'\n'+this.batteryPower+'\n'+this.price;
+    }
+    remainingAmount(priceIHave){
+        this.price = this.price - priceIHave;
+    }
+}
